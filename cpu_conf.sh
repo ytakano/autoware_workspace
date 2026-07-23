5a0eda59a2f40acb2e8fc7799373c14ef8867b6e
Yuuki Takano <ytakanoster@gmail.com>
2026-07-24 01:14:02 +0900

    add cpu_conf.*
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

---
+++ cpu_conf.sh
@@ -0,0 +1,11 @@
+cpupower frequency-set --governor performance
+
+for d in /sys/devices/system/cpu/cpu*/cpufreq; do
+	echo 3200000 | tee $d/scaling_max_freq
+	echo 3200000 | tee $d/scaling_min_freq
+done
+
+echo 0 | tee /sys/devices/system/cpu/cpu3/online
+#echo 0 | tee /sys/devices/system/cpu/cpu5/online
+#echo 0 | tee /sys/devices/system/cpu/cpu7/online
+#echo 0 | tee /sys/devices/system/cpu/cpu9/online
